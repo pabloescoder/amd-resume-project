0a0e5fb09ebfe34bdef327b0a831b1e71f5e99a7
Dhroov Makwana <pabloescoder@gmail.com>
2023-04-05 21:51:16 +0530

    Added homepage and made it responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import React, { useState } from "react";
+import Homepage from "./components/Homepage";
+
 function App() {
-  return <div className="App"></div>;
+  const [currentSection, setCurrentSection] = useState(0);
+
+  const incrementCurrentSection = () => {
+    setCurrentSection((prevValue) => prevValue + 1);
+  };
+
+  const decrementCurrentSection = () => {
+    setCurrentSection((prevValue) => prevValue + 1);
+  };
+
+  return (
+    <div className="App">
+      {currentSection === 0 && <Homepage nextPage={incrementCurrentSection} />}
+      {currentSection === 1 && <div>Hellooo</div>}
+    </div>
+  );
 }
 
 export default App;
