651b6a30ce8144831046f926046492ca8fff79b4
Suhrud Wani <suhrudwani1010@gmail.com>
2023-04-10 09:44:13 +0530

    Added config for cors, created a basic express server in server.js, added some default middleware

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+  "https://pabloescoder-resume-builder.vercel.app/",
+  "http://127.0.0.1:3000", // React Server (Development only)
+  "http://localhost:3500", // Express Server (Development only)
+];
+
+module.exports = allowedOrigins;
